11da74e7f2b43dee21c040f8fd2bb05a53763ac9
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-03-21 16:02:54 +0530

    Create rohitt212.c

---
+++ rohitt212.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#incllude<conio.h>
+
+
+
+int main(void) 
+
+{
+
+	int n;
+
+	printf("enter number:");
+
+	scanf("%d",&n);
+
+	if(n%13==0)
+
+	{
+
+		printf("\n yes");
+
+	}
+
+	else
+
+	{
+
+		printf("\n no");
+
+	}
+  getch();
+
+	return 0;
